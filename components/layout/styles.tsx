e7776da8028075b29060a852813f7f0c635765b9
Diego Camara <diegodscamara@gmail.com>
2022-10-28 21:49:47 -0400

    Add favicon, icons and variables

---
+++ components/layout/styles.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import theme from '../styles/theme'
+
+export const Wrapper = styled.main`
+  background-color: ${theme.colors.general.blue400};
+  padding: ${theme.spacing.small} ${theme.spacing.xsmall};
+`
+export const Container = styled.section``
+export const Children = styled.section``
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,10 @@
-import Head from 'next/head'
 import Link from 'next/link'
 
 export default function Home(): JSX.Element {
   return (
     <section>
-      <Head>
-        <title>Alura Dev</title>
-        <meta name="description" content="Alura Dev" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
-      </Head>
-
       <div>
-        <h1>
-          Welcome to Alura Dev!
-        </h1>
+        <h1>Welcome to Alura Dev!</h1>
 
         <article>
           <Link href={'/community'}>Community</Link>
